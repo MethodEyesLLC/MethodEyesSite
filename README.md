5d242e946e507cd8b4629d1e68c8da48421e02c8
mattbasile <matthewbasile2@gmail.com>
2023-05-10 13:08:32 -0400

    v1

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MethodEyesSite
 The website for Method Eyes
+
+
+## How to run locally
+Hi James! 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ['./**/*.{html,js}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Ubuntu', 'sans-serif'],
+      serif: ['Gloock', 'serif'],
+    },
+    extend: {
+      spacing: {
+        30: '120px',
+      },
+      maxWidth: {
+        '6.5xl': '1200px',
+      },
+      colors: {
+        coral: { 100: '#FCF8F7', 500: '#D5563D' },
+        midnight: '#0A1524',
+        heather: '#9E7EB9',
+        walnut: '#C7BEBC',
+      },
+      borderRadius: {
+        '4xl': '32px',
+      },
+      lineHeight: {
+        12: '54px',
+      },
+    },
   },
   plugins: [],
 };
